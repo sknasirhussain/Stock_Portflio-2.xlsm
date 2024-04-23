9180308ee0bec4631ff633b9cc789c1308f57bb2
sknasirhussain <128985191+sknasirhussain@users.noreply.github.com>
2024-04-23 16:28:48 +0530

    Adding New Module

---
+++ Clear.vb
@@ -0,0 +1,10 @@
+Option Explicit
+
+Sub clearStuff()
+
+    Range("D3:D22").ClearContents
+    Range("J3:K22").ClearContents
+    Range("C3:C22").ClearContents
+    Debug.Print "finishing clear script"
+
+End Sub
